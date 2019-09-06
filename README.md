87d688f428c5655e2e7fb142384b5e54381a92be
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-09-06 08:27:48 -0700

    Version 2.3.1 → 2.3.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pipes-Safe v2.3.1
+# Pipes-Safe
 
 `pipes-safe` builds upon
 [the `pipes` library](https://github.com/Gabriel439/Haskell-Pipes-Library) to
--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pipes-safe";
-  version = "2.3.1";
+  version = "2.3.2";
   src = ./.;
   libraryHaskellDepends = [
     base containers exceptions monad-control mtl pipes primitive
