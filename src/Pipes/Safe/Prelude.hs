e6b211d75589d3aae409a7bdd7953c97ee98a472
Chris Martin <ch.martin@gmail.com>
2022-03-29 13:36:01 -0600

    add allocate, allocate_, openFile

--- src/Pipes/Safe/Prelude.hs
+++ src/Pipes/Safe/Prelude.hs
@@ -5,16 +5,22 @@
 module Pipes.Safe.Prelude (
     -- * Handle management
     withFile,
+    openFile,
 
     -- * String I/O
     -- $strings
     readFile,
-    writeFile
+    writeFile,
+
+    -- * Registering/releasing
+    allocate,
+    allocate_
     ) where
 
+import Control.Monad.Catch (mask_)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Pipes (Producer', Consumer')
-import Pipes.Safe (bracket, MonadSafe)
+import Pipes.Safe (bracket, liftBase, register, Base, MonadSafe, ReleaseKey)
 import qualified Pipes.Prelude as P
 import qualified System.IO as IO
 import Prelude hiding (readFile, writeFile)
@@ -23,6 +29,16 @@ import Prelude hiding (readFile, writeFile)
 withFile :: (MonadSafe m) => FilePath -> IO.IOMode -> (IO.Handle -> m r) -> m r
 withFile file ioMode = bracket (liftIO $ IO.openFile file ioMode) (liftIO . IO.hClose)
 {-# INLINABLE withFile #-}
+
+{- | Acquire a 'IO.Handle' within 'MonadSafe'
+
+     The 'ReleaseKey' can be used to close the handle with 'Pipes.Safe.release';
+     otherwise the handle will be closed automatically at the conclusion of the
+     'MonadSafe' block.
+-}
+openFile :: MonadSafe m => FilePath -> IO.IOMode -> m (ReleaseKey, IO.Handle)
+openFile file ioMode = allocate (liftIO $ IO.openFile file ioMode) (liftIO . IO.hClose)
+{-# INLINABLE openFile #-}
 
 {- $strings
     Note that 'String's are very inefficient, and I will release future separate
@@ -44,3 +60,31 @@ readFile file = withFile file IO.ReadMode P.fromHandle
 writeFile :: MonadSafe m => FilePath -> Consumer' String m r
 writeFile file = withFile file IO.WriteMode $ \h -> P.toHandle h
 {-# INLINABLE writeFile #-}
+
+{- | Acquire some resource with a guarantee that it will eventually be released
+
+     The 'ReleaseKey' can be passed to 'Pipes.Safe.release' to
+     release the resource manually. If this has not been done by the end
+     of the 'MonadSafe' block, the resource will be released automatically.
+-}
+allocate :: MonadSafe m =>
+    Base m a             -- ^ Acquire
+    -> (a -> Base m ())  -- ^ Release
+    -> m (ReleaseKey, a)
+allocate acq rel = mask_ $ do
+    a <- liftBase acq
+    key <- register (rel a)
+    return (key, a)
+
+{- | Like 'allocate', but for when the resource itself is not needed
+
+     The acquire  action runs immediately. The 'ReleaseKey' can be passed
+     to 'Pipes.Safe.release' to run the release action. If this has not been
+     done by the end of the 'MonadSafe' block, the release action will be
+     run automatically.
+-}
+allocate_ :: MonadSafe m =>
+    Base m a        -- ^ Acquire
+    -> (Base m ())  -- ^ Release
+    -> m ReleaseKey
+allocate_ acq rel = fmap fst (allocate acq (const rel))
