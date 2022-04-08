a89b29402bcf777e3a8a4fbff2189e0e6c9f775d
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-04-07 18:31:53 -0700

    Version 2.3.3 → 2.3.4

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-{ mkDerivation, base, containers, exceptions, monad-control, mtl
-, pipes, primitive, stdenv, transformers, transformers-base
+{ mkDerivation, base, containers, exceptions, lib, monad-control
+, mtl, pipes, primitive, transformers, transformers-base
 }:
 mkDerivation {
   pname = "pipes-safe";
-  version = "2.3.3";
+  version = "2.3.4";
   src = ./.;
   libraryHaskellDepends = [
     base containers exceptions monad-control mtl pipes primitive
     transformers transformers-base
   ];
   description = "Safety for the pipes ecosystem";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
